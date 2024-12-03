7a7493165507828b555f64b3a22c1b3f8e9b2f42
Gagkuev Alexander <alex.gags2004@gmail.com>
2024-12-03 21:48:29 +0300

    Initial Commit

---
+++ redis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  redis
+//
+//  Created by Александр on 03.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ redis/redisApp.swift
@@ -0,0 +1,17 @@
+//
+//  redisApp.swift
+//  redis
+//
+//  Created by Александр on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct redisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
